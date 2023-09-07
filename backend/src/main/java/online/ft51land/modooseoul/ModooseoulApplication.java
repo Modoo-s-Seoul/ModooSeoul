66d317d47ca3e1018d916c670e17f47d9b8022b3
changkim1 <zzankor@naver.com>
2023-09-07 17:41:45 +0900

    [BE] init: 프로젝트 초기 세팅

---
+++ backend/src/main/java/online/ft51land/modooseoul/ModooseoulApplication.java
@@ -0,0 +1,13 @@
+package online.ft51land.modooseoul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModooseoulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModooseoulApplication.class, args);
+	}
+
+}
