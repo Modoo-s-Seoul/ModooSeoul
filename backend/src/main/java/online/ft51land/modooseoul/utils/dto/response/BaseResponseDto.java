426da23da6f666a893c7fd3a351fd9808a644e0c
dksek3050 <dksek3050@gmail.com>
2023-09-08 10:41:26 +0900

    [BE] feat: BaseResponse 생성

---
+++ backend/src/main/java/online/ft51land/modooseoul/utils/dto/response/BaseResponseDto.java
@@ -0,0 +1,16 @@
+package online.ft51land.modooseoul.utils.dto.response;
+
+
+public record BaseResponseDto<T>(String message, T data) {
+
+    public static <T> BaseResponseDto<T> ok(T data){
+        return new BaseResponseDto<>("success",data);
+    }
+    public static <T> BaseResponseDto<T> error(T data){
+        return new BaseResponseDto<>("error",data);
+    }
+
+    public static <T> BaseResponseDto<T> message(T data, String message){
+        return new BaseResponseDto<>(message, data);
+    }
+}
