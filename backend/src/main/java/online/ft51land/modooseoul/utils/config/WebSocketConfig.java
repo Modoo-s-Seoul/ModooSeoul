95567e861fb7ab96a3bd744c22b86f8b36422221
changkim1 <zzankor@naver.com>
2023-09-11 15:03:12 +0900

    [BE] fix: WebSocketConfig AllowedOrigin 변경 (* 으로)

--- backend/src/main/java/online/ft51land/modooseoul/utils/config/WebSocketConfig.java
+++ backend/src/main/java/online/ft51land/modooseoul/utils/config/WebSocketConfig.java
@@ -16,7 +16,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("ws").withSockJS(); // ws 엔드포인트 주소로 SockJS 를 사용하여 통신하겠다.
+		registry.addEndpoint("ws").setAllowedOrigins("*")
+		        .withSockJS(); // ws 엔드포인트 주소로 SockJS 를 사용하여 통신하겠다.
 	}
 
 	@Override
