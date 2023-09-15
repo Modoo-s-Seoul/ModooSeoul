84704d7b300358a3d289fc0af5a5b764b17f2027
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-09-15 14:52:28 +0900

    [FE] fix: RecoilRoot 삭제 - main.tsx에 이미 있어서 겹침

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,23 +6,20 @@ import Room from "./pages/home/Room";
 import RoomCreate from "./pages/home/RoomCreate";
 import RoomInvite from "./pages/home/RoomInvite";
 import { Route, Routes } from "react-router-dom";
-import { RecoilRoot } from "recoil";
 
 function App() {
   return (
     <>
-      <RecoilRoot>
-        <Routes>
-          {/* 홈 */}
-          <Route path="/home" element={<Lobby />} />
-          <Route path="/home/create" element={<RoomCreate />} />
-          <Route path="/home/random" element={<RandomMatch />} />
-          <Route path="/home/invite/:pk" element={<RoomInvite />} />
-          <Route path="/home/room" element={<Room />} />
-          {/* 인게임 */}
-          <Route path="/game" element={<MonopolyBoard />} />
-        </Routes>
-      </RecoilRoot>
+      <Routes>
+        {/* 홈 */}
+        <Route path="/home" element={<Lobby />} />
+        <Route path="/home/create" element={<RoomCreate />} />
+        <Route path="/home/random" element={<RandomMatch />} />
+        <Route path="/home/invite/:pk" element={<RoomInvite />} />
+        <Route path="/home/room" element={<Room />} />
+        {/* 인게임 */}
+        <Route path="/game" element={<MonopolyBoard />} />
+      </Routes>
     </>
   );
 }
