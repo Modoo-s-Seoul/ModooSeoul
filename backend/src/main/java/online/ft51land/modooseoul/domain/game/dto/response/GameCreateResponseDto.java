3528ba540a8c8470e0d870a3157758d98c8cac25
changkim1 <zzankor@naver.com>
2023-09-18 16:45:23 +0900

    [BE] fix: create Response Dto 수정

--- backend/src/main/java/online/ft51land/modooseoul/domain/game/dto/response/GameCreateResponseDto.java
+++ backend/src/main/java/online/ft51land/modooseoul/domain/game/dto/response/GameCreateResponseDto.java
@@ -3,22 +3,13 @@ package online.ft51land.modooseoul.domain.game.dto.response;
 import lombok.Builder;
 import online.ft51land.modooseoul.domain.game.entity.Game;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 @Builder
 public record GameCreateResponseDto(
-        String id,
-        List<String> players,
-        Boolean isStart,
-        LocalDateTime createDate
+        String id
 ) {
-    public static GameCreateResponseDto of(Game game){
+    public static GameCreateResponseDto of(Game game) {
         return GameCreateResponseDto.builder()
                                     .id(game.getId())
-                                    .players(game.getPlayers())
-                                    .isStart(game.getIsStart())
-                                    .createDate(game.getCreatedDate())
                                     .build();
     }
 }
