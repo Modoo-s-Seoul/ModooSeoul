f68aefb01e93112c6ed9df9b1593132c931d69bd
changkim1 <zzankor@naver.com>
2023-09-21 09:43:52 +0900

    [BE] fix: 서비스 호출 -> 컨트롤러 레이어로 이동

--- backend/src/main/java/online/ft51land/modooseoul/domain/game/controller/GameWebSocketController.java
+++ backend/src/main/java/online/ft51land/modooseoul/domain/game/controller/GameWebSocketController.java
@@ -6,10 +6,15 @@ import online.ft51land.modooseoul.domain.game.dto.message.GameStartMessage;
 import online.ft51land.modooseoul.domain.game.entity.Game;
 import online.ft51land.modooseoul.domain.game.service.GameService;
 import online.ft51land.modooseoul.domain.messagenum.service.MessageNumService;
+import online.ft51land.modooseoul.domain.player.entity.Player;
+import online.ft51land.modooseoul.domain.player.service.PlayerService;
 import online.ft51land.modooseoul.utils.websocket.WebSocketSendHandler;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -17,17 +22,20 @@ import org.springframework.stereotype.Controller;
 public class GameWebSocketController {
 
 	private final GameService gameService;
+	private final PlayerService playerService;
 	private final MessageNumService messageNumService;
 	private final WebSocketSendHandler webSocketSendHandler;
 
 	@MessageMapping("/start/{gameId}")
 	public void gameStart(@DestinationVariable String gameId) {
 		Game game = gameService.getGameById(gameId);
-
-		log.info("게임 시작 메시지 수령 by = {}", gameId);
+		List<Player> players = new ArrayList<>();
+		for (String playerId : game.getPlayers()) {
+			players.add(playerService.getPlayerById(playerId));
+		}
 
 		// 게임 시작 가능 여부 반환
-		GameStartMessage gameStartMessage = gameService.gameStart(game);
+		GameStartMessage gameStartMessage = gameService.gameStart(game, players);
 
 		Long messageNum = messageNumService.getMessageNumByGameId(gameId);
 
