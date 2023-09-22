7df08797cd92a0d644324f8a71f3f69c81c4ac56
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-09-22 13:04:43 +0900

    [FE] update: IngameModal 옵션 추가

--- frontend/src/components/Base/IngameModal.tsx
+++ frontend/src/components/Base/IngameModal.tsx
@@ -5,6 +5,10 @@ interface Props {
   children?: ReactNode;
   width?: string;
   height?: string;
+  maxWidth?: string;
+  maxHeight?: string;
+  minWidth?: string;
+  minHeight?: string;
   visible?: boolean;
 }
 
@@ -14,12 +18,23 @@ export default function IngameModal({
   width = "30vw",
   height = "60vh",
   visible = true,
+  maxWidth = "1200px",
+  maxHeight = "600px",
+  minWidth,
+  minHeight,
 }: Props) {
   return (
     <>
       <div
         className={`ingameModal ${visible ? "ingameModalActive" : ""}`}
-        style={{ width: `${width}`, height: `${height}` }}
+        style={{
+          width: `${width}`,
+          height: `${height}`,
+          maxWidth: `${maxWidth}`,
+          maxHeight: `${maxHeight}`,
+          minWidth: `${minWidth}`,
+          minHeight: `${minHeight}`,
+        }}
       >
         {children}
       </div>
