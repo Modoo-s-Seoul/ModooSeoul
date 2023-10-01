325f50508e5bd01ba5f70f687b753e9744cd182b
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-10-01 23:45:50 +0900

    [FE] design: 탈세 신고 컴포넌트 css 제작

--- frontend/src/components/CommonTurn/TaxThiefCatch.tsx
+++ frontend/src/components/CommonTurn/TaxThiefCatch.tsx
@@ -6,43 +6,76 @@ import { playerDataState } from "../../data/IngameData";
 
 export default function TaxThiefCatch() {
   const playerData = useRecoilValue(playerDataState);
-  const [selectedPlayer, setSelectedPlayer] = useState(6);
+  // const [selectedPlayer, setSelectedPlayer] = useState(6);
+  const [selected, setSelected] = useState(false);
 
   /**플레이어 신고 */
   const handleReport = () => {
-    if (selectedPlayer !== 6) {
-      console.log("선택한 플레이어:", selectedPlayer);
-    } else if (selectedPlayer == 6) {
-      alert("플레이어를 선택하세요.");
-    }
+    // if (selectedPlayer !== 6) {
+    //   console.log("선택한 플레이어:", selectedPlayer);
+    // } else if (selectedPlayer == 6) {
+    alert("플레이어를 선택하세요.");
+    // }
   };
 
   return (
     <div className="modalBaseContainer">
       <div className="modalBaseTitle">탈 세 신 고</div>
       <div className="modalBaseBody">
-        {playerData.map((player, index) => (
+        <div className="taxThiefGrid">
+          {playerData.map((player, index) => (
+            <div
+              key={index}
+              className={`modalBaseButton taxThiefButton ${
+                selected ? "centered" : ""
+              }`}
+              style={{
+                cursor: `${selected ? "" : "pointer"}`,
+              }}
+              onClick={() => setSelected(true)}
+              // onClick={() => {
+              //   if (selectedPlayer == index) {
+              //     setSelectedPlayer(6);
+              //   } else {
+              //     setSelectedPlayer(index);
+              //   }
+              // }}
+            >
+              {player.name}
+            </div>
+          ))}
+        </div>
+        <div className="reportButtonContainer">
           <div
-            key={index}
-            className="modalBaseButton"
-            onClick={() => {
-              if (selectedPlayer == index) {
-                setSelectedPlayer(6);
-              } else {
-                setSelectedPlayer(index);
-              }
-            }}
+            className="noticeBox"
+            style={{ transform: `${selected ? "scale(0)" : ""}` }}
           >
-            {player.name}
+            탈세자로 의심되는 사람을 고르세요.
           </div>
-        ))}
-        <button
-          className="reportButton"
-          style={{ cursor: "pointer" }}
-          onClick={handleReport}
-        >
-          신고하기
-        </button>
+          <button
+            className={`reportButton cancelButton ${
+              selected ? "cancelButtonVisible" : ""
+            }`}
+            style={{
+              cursor: "pointer",
+            }}
+            onClick={() => setSelected(false)}
+          >
+            다시 고르기
+          </button>
+          <button
+            className={`reportButton glowingButton ${
+              selected ? "reportButtonVisible" : ""
+            }
+            `}
+            style={{
+              cursor: "pointer",
+            }}
+            onClick={handleReport}
+          >
+            신고하기
+          </button>
+        </div>
       </div>
     </div>
   );
