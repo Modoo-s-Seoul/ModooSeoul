6b3d7ffec6b82dc28ae0082de5f096daa223ccde
An Na <dksek3050@gmail.com>
2023-10-01 21:47:19 +0900

    [BE] feat: synergy entity 구현

---
+++ backend/src/main/java/online/ft51land/modooseoul/domain/synergy/repository/SynergyReository.java
@@ -0,0 +1,8 @@
+package online.ft51land.modooseoul.domain.synergy.repository;
+
+import online.ft51land.modooseoul.domain.synergy.entity.Synergy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SynergyReository extends JpaRepository<Synergy, Long> {
+}
+
