cf97b3eec1185dcef29fe4412e54e25de79be4bd
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-10-02 10:25:53 +0900

    [FE] feat: 탈세 신고 컴포넌트 제작 완료

--- frontend/src/components/CommonTurn/TaxThiefCatch.tsx
+++ frontend/src/components/CommonTurn/TaxThiefCatch.tsx
@@ -2,20 +2,47 @@ import { useState } from "react";
 import { useRecoilValue } from "recoil";
 
 import "./TaxThiefCatch.css";
-import { playerDataState } from "../../data/IngameData";
+import { playerDataState, playerInfoState } from "../../data/IngameData";
+import { sendPlayerMessage } from "../IngameWs/IngameSendFunction";
+import { useSocket } from "../../pages/SocketContext";
 
 export default function TaxThiefCatch() {
+  const socketClient = useSocket();
+  const playerInfo = useRecoilValue(playerInfoState);
   const playerData = useRecoilValue(playerDataState);
-  // const [selectedPlayer, setSelectedPlayer] = useState(6);
   const [selected, setSelected] = useState(false);
+  const [reported, setReported] = useState(false);
+  const [taxThief, setTaxThief] = useState("");
+  const [taxThiefColor, setTaxThiefColor] = useState("");
 
-  /**플레이어 신고 */
+  /** 플레이어 선택 */
+  const selectPlayer = (playerName: string, playerColor: string) => {
+    setSelected(true);
+    setTaxThief(playerName);
+    setTaxThiefColor(playerColor);
+  };
+
+  /** 다시 고르기 */
+  const chooseAgain = () => {
+    setSelected(false);
+    setTaxThief("");
+    setTaxThiefColor("");
+  };
+
+  /** 플레이어 신고 */
   const handleReport = () => {
-    // if (selectedPlayer !== 6) {
-    //   console.log("선택한 플레이어:", selectedPlayer);
-    // } else if (selectedPlayer == 6) {
-    alert("플레이어를 선택하세요.");
-    // }
+    if (selected) {
+      sendPlayerMessage(
+        socketClient,
+        playerInfo.playerId,
+        "sent/report",
+        `{"reporteeName":${taxThief}}`
+      );
+      setReported(true);
+      console.log(`${taxThief} has been reported.`);
+    } else {
+      alert("플레이어를 선택하세요.");
+    }
   };
 
   return (
@@ -27,23 +54,44 @@ export default function TaxThiefCatch() {
             <div
               key={index}
               className={`modalBaseButton taxThiefButton ${
-                selected ? "centered" : ""
+                selected ? "selected" : ""
               }`}
               style={{
                 cursor: `${selected ? "" : "pointer"}`,
+                color: `#${player.color}`,
               }}
-              onClick={() => setSelected(true)}
-              // onClick={() => {
-              //   if (selectedPlayer == index) {
-              //     setSelectedPlayer(6);
-              //   } else {
-              //     setSelectedPlayer(index);
-              //   }
-              // }}
+              onClick={() => selectPlayer(player.name, player.color)}
             >
               {player.name}
             </div>
           ))}
+          <div
+            className={`questionBox ${
+              selected && !reported ? "selectedBoxVisible" : ""
+            }`}
+          >
+            <div>해당 플레이어를 탈세자로 신고하겠습니까?</div>
+            <div>
+              허위 신고 시 <span style={{ color: "red" }}>과태료</span>가
+              부과됩니다.
+            </div>
+          </div>
+          <div
+            className={`selectedBox ${
+              selected && !reported ? "selectedBoxVisible" : ""
+            }`}
+            style={{ color: `#${taxThiefColor}` }}
+          >
+            {taxThief}
+          </div>
+          <div
+            className={`questionBox ${
+              selected && reported ? "selectedBoxVisible" : ""
+            }`}
+            style={{ top: "40%", fontSize: "80px" }}
+          >
+            신고 완료!
+          </div>
         </div>
         <div className="reportButtonContainer">
           <div
@@ -54,18 +102,18 @@ export default function TaxThiefCatch() {
           </div>
           <button
             className={`reportButton cancelButton ${
-              selected ? "cancelButtonVisible" : ""
+              selected && !reported ? "cancelButtonVisible" : ""
             }`}
             style={{
               cursor: "pointer",
             }}
-            onClick={() => setSelected(false)}
+            onClick={chooseAgain}
           >
             다시 고르기
           </button>
           <button
             className={`reportButton glowingButton ${
-              selected ? "reportButtonVisible" : ""
+              selected && !reported ? "reportButtonVisible" : ""
             }
             `}
             style={{
