d99813d58d064831750e60b1579455c06e37e36d
안나 <dksek3050@gmail.com>
2023-10-05 00:42:57 +0900

    [BE] fix: 주식 변동 내역 저장

--- backend/src/main/java/online/ft51land/modooseoul/domain/game_stock/entity/GameStock.java
+++ backend/src/main/java/online/ft51land/modooseoul/domain/game_stock/entity/GameStock.java
@@ -7,6 +7,9 @@ import lombok.NoArgsConstructor;
 import online.ft51land.modooseoul.domain.stock.entity.Stock;
 import org.springframework.data.redis.core.RedisHash;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 @RedisHash(value = "game_stock") // Redis Repository 사용을 위한
 @NoArgsConstructor
@@ -21,13 +24,18 @@ public class GameStock {
 	@Column(name = "stock_name")
 	String stockName;
 
+	List<Long> stockPriceHistory;
+
 	public GameStock(Stock stock, String gameId) {
 		this.id = gameId + "@" + stock.getId();
 		this.stockPrice = stock.getPrice();
 		this.stockName = stock.getName();
+		this.stockPriceHistory = new ArrayList<>();
+		stockPriceHistory.add(stockPrice);
 	}
 
 	public void setStocksPrice(Long stockPrice) {
 		this.stockPrice = stockPrice;
+		stockPriceHistory.add(this.stockPrice);
 	}
 }
