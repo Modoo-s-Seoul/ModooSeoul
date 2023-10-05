454403a37351ddf1c29b547fb6b101b7be6466e1
조준하 <whwnsgk830@naver.com>
2023-10-05 13:07:24 +0900

    [FE] fix : 언마운트시 타이머 해제 요청

--- frontend/src/components/Turn/Ground.tsx
+++ frontend/src/components/Turn/Ground.tsx
@@ -174,14 +174,10 @@ export default function Ground() {
       `send/timer`,
       `{"timerType":"ESTATE_PURCHASE"}`
     );
-    // // 언마운트시 타이머 해제
-    // return () => {
-    //   sendWsMessage(
-    //     socketClient,
-    //     playerInfo.playerId,
-    //     "/send/timer-cancel"
-    //   );
-    // };
+    // 언마운트시 타이머 해제
+    return () => {
+      sendWsMessage(socketClient, playerInfo.playerId, "/send/timer-cancel");
+    };
   }, []);
 
   // 턴데이터 갱신
