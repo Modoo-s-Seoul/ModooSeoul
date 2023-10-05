c7360de006cb40b0b8b89939b919d64b36aae714
안나 <dksek3050@gmail.com>
2023-10-06 00:21:33 +0900

    [BE] fix: 오일랜드 수정

--- backend/src/main/java/online/ft51land/modooseoul/domain/board_status/dto/message/FTOilLandMessage.java
+++ backend/src/main/java/online/ft51land/modooseoul/domain/board_status/dto/message/FTOilLandMessage.java
@@ -6,11 +6,14 @@ import online.ft51land.modooseoul.domain.board_status.entity.BoardStatus;
 @Builder
 public record FTOilLandMessage(
         Long playerCash,
+        Long FTOilLandBoardId,
         BoardStatus boardStatus
+
 ) {
-    public static FTOilLandMessage of(Long playerCash, BoardStatus boardStatus){
+    public static FTOilLandMessage of(Long playerCash, BoardStatus boardStatus, Long FTOilLandBoardId){
         return FTOilLandMessage.builder()
                 .playerCash(playerCash)
+                .FTOilLandBoardId(FTOilLandBoardId)
                 .boardStatus(boardStatus)
                 .build();
     }
